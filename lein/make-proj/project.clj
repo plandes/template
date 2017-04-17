f4e476285c827dcdcc4343013f44528671a3e9a8
Paul Landes <landes@mailc.net>
2017-04-17 10:21:34 -0500

    add jdk -> log4j dev dep

--- lein/make-proj/project.clj
+++ lein/make-proj/project.clj
@@ -42,5 +42,6 @@
               :dependencies [[org.apache.logging.log4j/log4j-slf4j-impl "2.7"]
                              [org.apache.logging.log4j/log4j-1.2-api "2.7"]
                              [org.apache.logging.log4j/log4j-jcl "2.7"]
+                             [org.apache.logging.log4j/log4j-jul "2.7"]
                              [com.zensols/clj-append "1.0.5"]]}}
   :main ${package}.core)
