24a4437a9dea0cfd91efea6834e5c3fc7d52f0e5
Paul Landes <landes@mailc.net>
2018-02-22 08:59:40 -0600

    split off latex styles, make files, templates and art

--- tex/res/lib/make-idx.mk
+++
@@ -1,9 +0,0 @@
-OBJS_TO_DEL+=	$(TEX).ilg $(TEX).idx $(TEX).ind
-LATDEPS+=	$(TEX).ilg
-#NO_SECOND_RUN=	1
-
-$(TEX).ilg:	$(TEX).tex
-		$(LAT) $(TEX).tex
-		makeindex $(TEX).idx
-		$(LAT) $(TEX).tex
-		makeindex $(TEX).idx
