2b885726f3ecb7549e457126db52e997f16ef940
Paul Landes <landes@mailc.net>
2019-12-29 08:59:36 -0600

    maven test unit test template

--- maven/make-proj/test-dir/HelloWorldTest.java
+++ maven/make-proj/test-dir/HelloWorldTest.java
@@ -4,23 +4,27 @@ package ${package};
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
+
+import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertFalse;
 
 public class HelloWorldTest {
     private static final Log log = LogFactory.getLog(HelloWorldTest.class);
 
-    @BeforeClass
-    public static void setup() {
+    @Before
+    public void setup() {
 	if (log.isDebugEnabled()) {
 	    log.debug("setting up...");
 	}
     }
 
-    @AfterClass
-    public static void tearDown() {
+    @After
+    public void tearDown() {
 	if (log.isDebugEnabled()) {
 	    log.debug("tearing down...");
 	}
@@ -28,6 +32,6 @@ public class HelloWorldTest {
 
     @Test
     public void testHelloWorld() throws Exception {
-        Assert.assertEquals(1, 1);
+        assertEquals(1, 1);
     }
 }
