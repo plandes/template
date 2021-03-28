e92bcff38844b37879055115b4451de2889ad7dd
Paul Landes <landes@mailc.net>
2021-03-28 11:25:45 -0500

    bump out source to separate dir; cleanup

--- python-small/make-proj/entry.py
+++ python-small/make-proj/entry.py
@@ -13,18 +13,18 @@ from zensols.cli import ApplicationFactory
 class ${appclass}Factory(ApplicationFactory):
     @classmethod
     def instance(cls: type, root_dir: Path = Path('.'), *args, **kwargs):
-        dconf = DictionaryConfig(
-            {'appenv': {'root_dir': str(root_dir)},
-             'financial': {'salary': 15.}})
+        dconf = DictionaryConfig({'appenv': {'root_dir': str(root_dir)}})
         return cls(package_resource='${namespace}.${appshortname}',
                    children_configs=(dconf,))
 
 
 if __name__ == '__main__':
     entry_path = Path(sys.argv[0])
+    src_path = entry_path.parent / 'src'
     conf_path = entry_path.parent / 'etc' / '${project}.conf'
     if not conf_path.exists():
         conf_path = Path(os.environ['${environ}RC'])
     args = sys.argv + ['-c', str(conf_path)]
+    sys.path.append(str(src_path))
     cli = ${appclass}Factory.instance()
     cli.invoke(args[1:])
