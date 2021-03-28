3699860d4ad67f2762b0da1fc14817d4546bc7ac
Paul Landes <landes@mailc.net>
2021-03-28 17:40:26 -0500

    python-simple: add prototype capability

--- python-small/make-proj/entry.py
+++ python-small/make-proj/entry.py
@@ -12,13 +12,13 @@ from zensols.cli import ApplicationFactory
 @dataclass
 class ${appclass}Factory(ApplicationFactory):
     @classmethod
-    def instance(cls: type, root_dir: Path = Path('.'), *args, **kwargs):
+    def instance(cls: type, root_dir: Path = Path('.'), **kwargs):
         dconf = DictionaryConfig({'appenv': {'root_dir': str(root_dir)}})
         return cls(package_resource='${namespace}.${appshortname}',
-                   children_configs=(dconf,))
+                   children_configs=(dconf,), **kwargs)
 
 
-if __name__ == '__main__':
+def main(**factory_kwargs):
     entry_path = Path(sys.argv[0])
     src_path = entry_path.parent / 'src'
     conf_path = entry_path.parent / 'etc' / '${project}.conf'
@@ -26,5 +26,19 @@ if __name__ == '__main__':
         conf_path = Path(os.environ['${environ}RC'])
     args = sys.argv + ['-c', str(conf_path)]
     sys.path.append(str(src_path))
-    cli = ${appclass}Factory.instance()
+    cli = ${appclass}Factory.instance(**factory_kwargs)
     cli.invoke(args[1:])
+
+
+def proto():
+    print('')
+    main(reload_factory=True)
+
+
+if (__name__ == '__main__'):
+    # when running from a shell, run the CLI entry point
+    if 'SHLVL' in os.environ:
+        main()
+    # otherwise, assume a Python REPL and run the prototyping method
+    else:
+        proto()
