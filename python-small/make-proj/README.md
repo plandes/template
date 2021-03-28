b5d5c3264a57e9512699a48c024250dd8f8f5c7c
Paul Landes <landes@mailc.net>
2021-03-28 11:03:32 -0500

    add small python zensols based project

---
+++ python-small/make-proj/README.md
@@ -0,0 +1,16 @@
+#set($dateYear= $dateTool.get('yyyy'))
+# ${project-name}
+
+${project-description}
+
+
+#[[## License]]#
+
+[MIT License]
+
+Copyright (c) ${dateYear} ${user-name}
+
+
+<!-- links -->
+
+[MIT License]: https://opensource.org/licenses/MIT
---
+++ python-small/make-proj/entry.py
@@ -0,0 +1,30 @@
+#set ( $environ = $project.toUpperCase() )
+#!/usr/bin/env python
+
+from dataclasses import dataclass
+import sys
+import os
+from pathlib import Path
+from zensols.config import DictionaryConfig
+from zensols.cli import ApplicationFactory
+
+
+@dataclass
+class ${appclass}Factory(ApplicationFactory):
+    @classmethod
+    def instance(cls: type, root_dir: Path = Path('.'), *args, **kwargs):
+        dconf = DictionaryConfig(
+            {'appenv': {'root_dir': str(root_dir)},
+             'financial': {'salary': 15.}})
+        return cls(package_resource='${namespace}.${appshortname}',
+                   children_configs=(dconf,))
+
+
+if __name__ == '__main__':
+    entry_path = Path(sys.argv[0])
+    conf_path = entry_path.parent / 'etc' / '${project}.conf'
+    if not conf_path.exists():
+        conf_path = Path(os.environ['${environ}RC'])
+    args = sys.argv + ['-c', str(conf_path)]
+    cli = ${appclass}Factory.instance()
+    cli.invoke(args[1:])
