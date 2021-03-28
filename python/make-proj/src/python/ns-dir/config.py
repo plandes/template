f2f2301f7116e1461f6d1dff8dd735fc1da82896
Paul Landes <landes@mailc.net>
2021-03-28 10:07:53 -0500

    python: cleanup

--- python/make-proj/src/python/ns-dir/config.py
+++ python/make-proj/src/python/ns-dir/config.py
@@ -7,21 +7,18 @@ __author__ = 'Paul Landes'
 
 from dataclasses import dataclass
 from pathlib import Path
-from zensols.config import DictionaryConfig, EnvironmentConfig
+from zensols.config import DictionaryConfig
 from zensols.cli import ApplicationFactory
 
 
 @dataclass
 class ${prog}ApplicationFactory(ApplicationFactory):
     @classmethod
-    def instance(cls, root_dir: Path = Path('.'),
+    def instance(cls: type, root_dir: Path = Path('.'),
                  reload_factory: bool = False,
                  *args, **kwargs) -> ${prog}ApplicationFactory:
         dconf = DictionaryConfig(
             {'appenv': {'root_dir': str(root_dir)}})
-        econf = EnvironmentConfig(
-            section_name='env',
-            includes=set('HOME'.split()))
         return cls(package_resource='${namespace}',
-                   children_configs=(dconf, econf),
+                   children_configs=(dconf,),
                    reload_factory=reload_factory)
