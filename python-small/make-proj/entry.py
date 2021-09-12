6024555b848f27e652e7cc10a442c99004971c2b
Paul Landes <landes@mailc.net>
2021-09-12 11:27:59 -0500

    python: fix arg handling on python small

--- python-small/make-proj/entry.py
+++ python-small/make-proj/entry.py
@@ -1,6 +1,7 @@
 #set ( $environ = $project.toUpperCase() )
 #!/usr/bin/env python
 
+from typing import List, Dict, Any
 from dataclasses import dataclass
 import sys
 import os
@@ -19,13 +20,13 @@ class ${appclass}Factory(ApplicationFactory):
                    children_configs=(dconf,), **kwargs)
 
 
-def main(**factory_kwargs):
-    entry_path = Path(sys.argv[0])
+def main(args: List[str], **factory_kwargs: Dict[str, Any]):
+    entry_path = Path(args[0])
     src_path = entry_path.parent / 'src'
     conf_path = entry_path.parent / 'etc' / '${project}.conf'
     if not conf_path.exists():
         conf_path = Path(os.environ['${environ}RC'])
-    args = sys.argv + ['-c', str(conf_path)]
+    args = args + ['-c', str(conf_path)]
     sys.path.append(str(src_path))
     cli = ${appclass}Factory.instance(entry_path.parent, **factory_kwargs)
     cli.invoke(args[1:])
@@ -34,7 +35,7 @@ def main(**factory_kwargs):
 def proto():
     print('-->proto')
     try:
-        main(reload_factory=True)
+        main('_ proto'.split(), reload_pattern=r'^${namespace}')
     except SystemExit as e:
         print(f'exit: {e}')
 
@@ -43,7 +44,7 @@ if (__name__ == '__main__'):
     # when running from a shell, run the CLI entry point
     import __main__ as mmod
     if hasattr(mmod, '__file__'):
-        main()
+        main(sys.argv)
     # otherwise, assume a Python REPL and run the prototyping method
     else:
         proto()
