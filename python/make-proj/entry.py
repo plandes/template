bfa92871de5a64d5836ab4f028240992915edc49
Paul Landes <landes@mailc.net>
2022-06-13 11:27:39 -0500

    python template: CLI guard on __main__

--- python/make-proj/entry.py
+++ python/make-proj/entry.py
@@ -3,11 +3,12 @@
 
 from zensols.cli import ConfigurationImporterCliHarness
 
-harness = ConfigurationImporterCliHarness(
-    src_dir_name='src/python',
-    app_factory_class='${namespace}.ApplicationFactory',
-    config_path='test-resources/${project}.conf',
-    proto_args='doit',
-    proto_factory_kwargs={'reload_pattern': r'^${namespace}'},
-)
-harness.run()
+if (__name__ == '__main__'):
+    harness = ConfigurationImporterCliHarness(
+        src_dir_name='src/python',
+        app_factory_class='${namespace}.ApplicationFactory',
+        config_path='test-resources/${project}.conf',
+        proto_args='doit',
+        proto_factory_kwargs={'reload_pattern': r'^${namespace}'},
+    )
+    harness.run()
